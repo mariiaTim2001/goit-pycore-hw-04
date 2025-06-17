467558b65f164c166e904b56588b84d9c4c624b7
Mariia Timofieieva <30m2001m@gmail.com>
2025-06-17 15:53:29 +0300

    Add task1

---
+++ Task_1/main.py
@@ -0,0 +1,24 @@
+def total_salary(path):
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            salaries = []
+            for line in file:
+                try:
+                    salary = float(line.strip().split(',')[1])
+                    salaries.append(salary)
+                except (IndexError, ValueError):
+                    print(f"`Not correct format of line: {line.strip()}`")
+                    continue
+
+        total = sum(salaries)
+        average = total / len(salaries) if salaries else 0
+        return total, average
+
+    except FileNotFoundError:
+        print(f"File {path} does not exist.")
+        return 0, 0
+
+
+# Test case
+# total, average = total_salary("Task_1/file.txt")
+# print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
